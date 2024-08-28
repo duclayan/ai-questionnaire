8347a2b6ba8b4cd9ca0d83ab9bb4d0147b461691
dduclayan <dduclayan@deloitte.de>
2024-08-28 20:50:04 +0200

    Temporary hide dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Form from './views/Form/Form';
 import Login from './views/Login/Login';
-import Dashboard from './views/Dashboard/Dashboard';
+// import Dashboard from './views/Dashboard/Dashboard';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         <Route path="/form" element={<Form />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project/:id" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        {/* <Route path="/project/:id" element={<Dashboard />} /> */}
       </Routes>
     </Router>
   );
