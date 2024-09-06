6ba5053bc6e21192e2c0cd884855e96fba3145c2
dduclayan <dduclayan@deloitte.de>
2024-09-06 07:47:52 +0200

    Update to see logs

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -35,6 +35,18 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://dduclayan-frontend-egacbucchbcgfhd8.eastus-01.azurewebsites.net",
+]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -64,24 +76,40 @@ for pair in conn_str.split(' '):
         print(f"Warning: Skipping malformed pair: {pair}")
 
 # Define the DATABASES dictionary for Django
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': conn_str_params.get('dbname'),
-#         'HOST': conn_str_params.get('host'),
-#         'USER': conn_str_params.get('user'),
-#         'PASSWORD': conn_str_params.get('password'),
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': conn_str_params.get('dbname'),
+        'HOST': conn_str_params.get('host'),
+        'USER': conn_str_params.get('user'),
+        'PASSWORD': conn_str_params.get('password'),
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
 
-CORS_ALLOWED_ORIGINS = [
-    "https://dduclayan-frontend-egacbucchbcgfhd8.eastus-01.azurewebsites.net",
-]
+ADMINS = [("user-rnd", "duclayandominique@mail.com")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = 'default from email'
