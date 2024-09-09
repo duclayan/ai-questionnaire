06a4cbb99a71b028d0e740c66689e247eda84957
dduclayan <dduclayan@deloitte.de>
2024-09-09 10:02:46 +0200

    add login auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Form from './views/Form/Form';
 import Login from './views/Login/Login';
 import Dashboard from './views/Dashboard/Dashboard';
+import { AuthProvider } from './components/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        <Route path="/form" element={<Form />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/project/:id" element={<Dashboard />} /> */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/form" element={<ProtectedRoute element={<Form />} />} />
+          <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+          {/* <Route path="/project/:id" element={<Dashboard />} /> */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,46 +1,82 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../components/AuthContext';
+import { Alert, Box, Button, Container, Snackbar, TextField, Typography } from '@mui/material';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const { login } = useAuth();
+
   const navigate = useNavigate();
+  const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('${apiEndpoint}/login/', { username, password });
+      const response = await axios.post(`${apiEndpoint}/login/`, { username, password });
       localStorage.setItem('token', response.data.token);
+      login()
       navigate('/form');
     } catch (error) {
+      setError('Incorrect username or password');
+      setOpenSnackbar(true);
       console.error('Login failed:', error);
     }
   };
-
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <> {console.log( process.env.REACT_APP_MODE)}</>
-      <form onSubmit={handleSubmit} style={{ width: '300px' }}>
-        <h2>Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
+    <Container 
+      maxWidth="xs" 
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
+    >
+      <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Login
+        </Typography>
+
+        {/* Error message positioned below the heading */}
+        {error && (
+          <Alert severity="error" variant="filled" style={{ marginBottom: '16px' }}>
+            {error}
+          </Alert>
+        )}
+
+        <TextField
+          label="Username"
+          variant="outlined"
+          fullWidth
+          margin="normal"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          style={{ width: '100%', marginBottom: '10px', padding: '5px' }}
         />
-        <input
+        <TextField
+          label="Password"
           type="password"
-          placeholder="Password"
+          variant="outlined"
+          fullWidth
+          margin="normal"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          style={{ width: '100%', marginBottom: '10px', padding: '5px' }}
         />
-        <button type="submit" style={{ width: '100%', padding: '5px' }}>Login</button>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          style={{ padding: '10px' }}
+        >
+          Login
+        </Button>
       </form>
-    </div>
+    </Container>
   );
-}
+};
 
 export default Login;
