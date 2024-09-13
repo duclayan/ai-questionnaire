ccc9a2eaae588542f56240ba2b832256e930d8c6
dduclayan <dduclayan@deloitte.de>
2024-09-13 07:51:40 +0200

    Change backend title, header and Index

--- backend/crud/urls.py
+++ backend/crud/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from api.views import *  # Import your view
 
+admin.site.site_title = "Blanco GPT Admin"
+admin.site.site_header = "Blanco GPT Administration"
+admin.site.index_title = "Welcome to Blanco GPT Admin"
+
 urlpatterns = [  # This is the correct variable name
     path("admin/", admin.site.urls),  # Admin path
     path("", include('api.urls')),  # Root path for ReactView
