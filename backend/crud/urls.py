1f1037fa787ae9e8f9c465b15d295eeee5b99940
dduclayan <dduclayan@deloitte.de>
2024-09-17 13:07:59 +0200

    fix django routing

--- backend/crud/urls.py
+++ backend/crud/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
 from api.views import *  # Import your view
 
 admin.site.site_title = "Blanco GPT Admin"
 admin.site.site_header = "Blanco GPT Administration"
 admin.site.index_title = "Welcome to Blanco GPT Admin"
 
-urlpatterns = [  # This is the correct variable name
-    path("", admin.site.urls),  # Admin path
-    path("api/", include('api.urls')),  # Root path for ReactView
+urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),  # Redirect root to admin
+    path('admin/', admin.site.urls),  # Admin path
+    path('api/', include('api.urls')),  # API paths
 ]
