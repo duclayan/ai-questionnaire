0f1b80a84dd67ba069e598d83763d254abdf7659
dduclayan <dduclayan@deloitte.de>
2024-09-18 11:57:12 +0200

    add default secret_key

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -3,7 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 from datetime import timedelta
 
-SECRET_KEY = os.environ['SECRET',"5HihHVOFCc"]
+SECRET_KEY = os.environ['SECRET_KEY',]
 
 allowed_hosts = [
     os.environ.get('WEBSITE_HOSTNAME'),
